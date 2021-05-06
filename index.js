d890971e2b2ad0400490546bf1f89271cad8f8fd
Donovan Moses <donovanjmoses@macbook-pro.lan>
2021-05-06 17:09:41 -0500

    Add GET request handlers

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+// project setup and load modules
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => { // this callback is called "route handler" in node.js
-    res.send("Hello World");
+// route handler
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/api/courses", (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000...")
 });
